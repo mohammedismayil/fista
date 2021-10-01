c1ae10474938257570feee4814ae33f034994a87
ismayil <mohammedismayil10@gmail.com>
2021-10-01 08:22:59 +0530

    added one label

--- lib/Home/Home.dart
+++ lib/Home/Home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'Row.dart';
-
 class ListViewHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,18 +21,55 @@ class ListViewHome extends StatelessWidget {
           "Mohabbat",
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
         ),
-        Text(
-          "Hello world",
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-        ),
-        TextButton(
-          style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-          ),
-          onPressed: () {},
-          child: Text('Click Here'),
-        )
-        
+        //  Row(
+        //   children: [
+        //     Image.asset(
+        //       '/assets/images/profile.jpg',
+        //       height: 100,
+        //       width: 30,
+        //       fit: BoxFit.fill,
+        // ),
+        //    ],
+        // ),
+        // Text(
+        //   "Hello world",
+        //   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        // ),
+        // TextButton(
+        //   style: ButtonStyle(
+        //     foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+        //   ),
+        //   onPressed: () {},
+        //   child: Text('Click Here'),
+        // ),
+        // ListView(
+        //   // This next line does the trick.
+        //   // shrinkWrap: true, //just set this property
+        //   // padding: const EdgeInsets.all(50),
+        //   scrollDirection: Axis.horizontal,
+        //   children: <Widget>[
+        //     Container(
+        //       width: 160.0,
+        //       color: Colors.red,
+        //     ),
+        //     Container(
+        //       width: 160.0,
+        //       color: Colors.blue,
+        //     ),
+        //     Container(
+        //       width: 160.0,
+        //       color: Colors.green,
+        //     ),
+        //     Container(
+        //       width: 160.0,
+        //       color: Colors.yellow,
+        //     ),
+        //     Container(
+        //       width: 160.0,
+        //       color: Colors.orange,
+        //     ),
+        //   ],
+        // )
       ],
     ));
   }
