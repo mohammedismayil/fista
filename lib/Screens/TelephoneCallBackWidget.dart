e8bd7a8a1bce8b2f3b6e7c23038284518ae05d92
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-09-23 07:39:49 +0530

    abtraction added for button disabling

--- lib/Screens/TelephoneCallBackWidget.dart
+++ lib/Screens/TelephoneCallBackWidget.dart
@@ -1,18 +1,55 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:flutterdemo/Screens/APIInteractor/AlbumModel.dart';
+import 'package:http/http.dart' as http;
 
 typedef SimpleCallBack = Function(int);
-class TelephoneCallBackWidget extends StatefulWidget {
+
+abstract class DisablingButton {
+  bool isDisabled = false;
+}
+
+class TelephoneCallBackWidget extends StatefulWidget
+    implements DisablingButton {
   TelephoneCallBackWidget({Key? key, this.informThem}) : super(key: key);
 
   SimpleCallBack? informThem;
   @override
   State<TelephoneCallBackWidget> createState() =>
       _TelephoneCallBackWidgetState();
+
+  @override
+  bool isDisabled = false;
 }
 
 class _TelephoneCallBackWidgetState extends State<TelephoneCallBackWidget> {
   int _count = 0;
+
+  void indicateDisabled() {
+    print("Nope i am disabled");
+  }
+
+  void incrementCount() async {
+    setState(() {
+      widget.isDisabled = true;
+    });
+
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+    if (response.statusCode == 200) {
+      _count = _count + 1;
+      widget.informThem!(_count);
+      widget.isDisabled = false;
+      setState(() {});
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,10 +57,7 @@ class _TelephoneCallBackWidgetState extends State<TelephoneCallBackWidget> {
         children: [
           Text("Telephone widget"),
           TextButton(
-              onPressed: () {
-                _count = _count + 1;
-                widget.informThem!(_count);
-              },
+              onPressed: widget.isDisabled ? indicateDisabled : incrementCount,
               child: Text("Call"))
         ],
       ),
